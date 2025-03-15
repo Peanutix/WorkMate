06179564672007343aa6e486aca050a5e939eae4
Pengu-8 <123yeeheng.com@gmail.com>
2025-03-15 21:01:29 +1100

    readded the logo to the login

--- workmate/src/pages/Login.jsx
+++ workmate/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import logo from "../assets/workmate_logo2.png";
 import { useNavigate } from "react-router-dom";
 
 function Login({ socket, setUsername, isConnected }) {
@@ -26,24 +27,25 @@ function Login({ socket, setUsername, isConnected }) {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-blue-500">
-      <div className="bg-white p-5 rounded-lg shadow-md w-80">
-        <h2 className="text-2xl font-bold text-center mb-4">Login</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          value={localUsername}
-          onChange={(e) => setLocalUsername(e.target.value)}
-          className="w-full p-2 my-2 border border-gray-300 rounded-md text-base"
-        />
-        <button
-          className="w-full p-2 mt-2 bg-blue-600 text-white rounded-md text-base font-semibold hover:bg-blue-700"
-          onClick={handleLogin}
-        >
-          Sign In
-        </button>
+      <div className="flex flex-col justify-center items-center h-screen bg-blue-500">
+          <img className="w-[350px] mb-6" src={logo} alt = "workmate"/>
+        <div className="bg-white p-5 rounded-lg shadow-md w-80">
+          <h2 className="text-2xl font-bold text-center mb-4">Login</h2>
+          <input
+              type="text"
+              placeholder="Username"
+              value={localUsername}
+              onChange={(e) => setLocalUsername(e.target.value)}
+              className="w-full p-2 my-2 border border-gray-300 rounded-md text-base"
+          />
+          <button
+              className="w-full p-2 mt-2 bg-blue-600 text-white rounded-md text-base font-semibold hover:bg-blue-700"
+              onClick={handleLogin}
+          >
+            Sign In
+          </button>
+        </div>
       </div>
-    </div>
   );
 }
 
